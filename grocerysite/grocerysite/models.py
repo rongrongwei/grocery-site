115e71b9b6069df0327a686dd2ed1acee701bfe8
Rongrong <rongrong@rongrongsMac.local>
2020-11-27 16:10:00 -0600

    reverted back some changes

--- grocerysite/grocerysite/models.py
+++ grocerysite/grocerysite/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from grocerysite.grocerysite import settings
+# from grocerysite.grocerysite import settings
 
 
 class Product(models.Model):
@@ -17,39 +17,35 @@ class Product(models.Model):
         db_table = 'product'
 
 #way of capturing what the user actually owns so we use a manytomanyfield
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.username
-
-#one order consists of many order items
-class OrderItem(models.Model):
-    Product = models.OneToOneField(Product, on_delete=models.SET_NULL, null =True)
-    is_ordered = models.BooleanField(default=False)
-    date_ordered = models.DateTimeField(null=True)
-    date_added = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.product.product_name
-
-class Order(models.Model):
-    ref_code=models.CharField(max_length=15)
-    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
-    is_ordered = models.BooleanField(default=False)
-    items = models.ManyToManyField(OrderItem)
-    date_ordered = models.DateTimeField(auto_now=True)
-
-    def get_cart_items(self):
-        return self.items.all()
-
-    def get_cart_total(self):
-        return sum([item.product.price for item in self.items.all()])
-
-
-
-
-
-
-
-
+# class Profile(models.Model):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+# 
+#     def __str__(self):
+#         return self.user.username
+# 
+# #one order consists of many order items
+# class OrderItem(models.Model):
+#     Product = models.OneToOneField(Product, on_delete=models.SET_NULL, null =True)
+#     is_ordered = models.BooleanField(default=False)
+#     date_ordered = models.DateTimeField(null=True)
+#     date_added = models.DateTimeField(auto_now=True)
+# 
+#     def __str__(self):
+#         return self.product.product_name
+# 
+# class Order(models.Model):
+#     ref_code=models.CharField(max_length=15)
+#     owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+#     is_ordered = models.BooleanField(default=False)
+#     items = models.ManyToManyField(OrderItem)
+#     date_ordered = models.DateTimeField(auto_now=True)
+# 
+#     def get_cart_items(self):
+#         return self.items.all()
+# 
+#     def get_cart_total(self):
+#         return sum([item.product.price for item in self.items.all()])
+# 
+# 
+# 
+# 
