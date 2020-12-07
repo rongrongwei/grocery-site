9966d10ae1f809c98a6f75dd6f3a83080abf2fa5
ChristopherUrista <58087450+ChristopherUrista@users.noreply.github.com>
2020-12-07 04:10:33 -0600

    some changes to login, user, search, search results

--- grocerysite/grocerysite/forms.py
+++ grocerysite/grocerysite/forms.py
@@ -4,7 +4,7 @@ from django import forms
 
 
 class Loginform(forms.Form):
-  username= forms.CharField(max_length= 25,label="Enter username")
+  username= forms.CharField(max_length= 25,label="Username")
   password= forms.CharField(max_length= 30, label='Password', widget=forms.PasswordInput)
 
 
